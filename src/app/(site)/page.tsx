3fc7b88c359bfb6ef466d63c20427e04c5047a7f
Rajsai1609 <rajsainaredla114@gmail.com>
2026-08-17 11:54:23 -0700

    Remove stats strip from homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,4 @@
 import { Hero }                  from "@/components/home/Hero";
-import { ClientLogos }            from "@/components/home/ClientLogos";
 import { ServiceCards }           from "@/components/home/ServiceCards";
 import { IndustryGrid }           from "@/components/home/IndustryGrid";
 import { WelcomeSection }         from "@/components/home/WelcomeSection";
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <ClientLogos />
       <ServiceCards />
       <IndustryGrid />
       {/* Keep existing trust / values / CTA sections */}
